2f468d9ae13076caa3b7050c34146185f1712d4c
Marcos Amorim <markstm10@gmail.com>
2021-12-01 21:03:18 -0300

    feat: created _document to set font and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
+import Head from "next/head";
+
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>Wosrld!</span>
+      </h1>
+    </>
+  );
 }
