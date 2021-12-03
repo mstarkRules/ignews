e627f5755b46ddb9a207d1a39d10efc69de77a5b
Marcos Amorim <markstm10@gmail.com>
2021-12-03 17:41:51 -0300

    style: created global style and seted responsiv sz

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>Wosrld!</span>
-      </h1>
+      <h1>Hello World!</h1>
     </>
   );
 }
