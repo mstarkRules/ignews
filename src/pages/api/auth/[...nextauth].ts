cf38971bf3a712107c7a56e85710b10be8400b6c
Marcos Amorim <markstm10@gmail.com>
2022-01-06 22:23:33 -0300

    feat: NextAuth - seted next auth and github configs

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,18 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
-  //configure one or more authentication providers
+  // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
     }),
     // ...add more providers here
   ],
 });
-
-// a database is optional, but required to persist accounts in a database
-database: process.env.DATABASE_URL;
