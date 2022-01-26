66bbbb364f475fb0b0b6c1ec2660b479c389871b
Marcos Amorim <markstm10@gmail.com>
2022-01-26 14:03:43 -0300

    feat: subscribe route creates a stripeCheckoutSession

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from "next-auth/react";
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +7,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    //create checkout session
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
