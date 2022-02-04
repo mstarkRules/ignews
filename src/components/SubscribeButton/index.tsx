d2485c092a344e4089ab11a5425742b77573b490
Marcos Amorim <markstm10@gmail.com>
2022-02-04 00:58:42 -0300

    feat: configured stripe checkout session and redirected

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 import styles from "./styles.module.scss";
 
@@ -9,13 +11,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
-
     //create checkout session
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
   }
   return (
     <button
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -23,6 +23,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
+    console.log("cheguei gml? ", stripeCheckoutSession.id);
     return res.status(200).json({
       sessionId: stripeCheckoutSession.id,
     });
