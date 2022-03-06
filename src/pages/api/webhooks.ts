e1aed35e1db3f11770141acfb99a7a0c6cec4e68
Marcos Amorim <markstm10@gmail.com>
2022-03-05 22:51:38 -0300

    feat: created function to save subscription on db

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -41,9 +42,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      //do something
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
 
-      console.log("Eventos recebidos: ", event);
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error("Unhadled event");
+        }
+      } catch (error) {
+        return res.json({ error: "Webhook handler failed" });
+      }
     }
 
     res.json({ ok: true });
